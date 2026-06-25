10eb212b9e6de1ede33de79de5b4a58ca78dc3e6
shrikrishna2507 <shrikrishnas2005@gmail.com>
2026-06-25 11:02:41 +0530

    Update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: https://gardenfresh-secg.onrender.com,
   headers: { 'Content-Type': 'application/json' },
 });
 
